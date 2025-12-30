f72989bc042e4e395255ca9d5ed77ac62daac88b
fasharifi <sfsh1.sharifi@gmail.com>
2025-12-30 16:45:52 +0330

    Implement Note model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,12 @@
+import uuid
+
 from django.db import models
+from books.models import Book
 
 # Create your models here.
+
+class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
