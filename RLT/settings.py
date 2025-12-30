7b16598a82406ceb26e4e3514b52a8cad581c227
fasharifi <sfsh1.sharifi@gmail.com>
2025-12-30 16:27:58 +0330

    Create Apps

--- RLT/settings.py
+++ RLT/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'books',
+    'goals',
+    'notes',
+    'sessions',
 ]
 
 MIDDLEWARE = [
