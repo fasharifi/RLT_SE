8d1ee67dd6fb78c8a9dff53f2b829d3a298e256d
fasharifi <sfsh1.sharifi@gmail.com>
2026-01-05 22:45:51 +0330

    Implement Goal models

--- goals/models.py
+++ goals/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from accounts.models import Account
+from books.models import Book
 
 
 # Create your models here.
@@ -11,3 +12,7 @@ class Goal(models.Model):
     progress = models.DecimalField(max_digits=5, decimal_places=2)
     deadline = models.DateField()
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+class GoalBook(models.Model):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
