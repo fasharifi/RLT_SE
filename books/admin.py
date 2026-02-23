c58afa46867d285b3aa6e763cdbb214d8bc6c2b2
fasharifi <sfsh1.sharifi@gmail.com>
2026-02-24 00:58:35 +0330

    edit books.admin

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from books.models import Book, Category
+from books.models import Book, Category, Role, Contributor, BookContribution, ReadingList
 
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(Category)
+admin.site.register(Role)
+admin.site.register(Contributor)
+admin.site.register(BookContribution)
+admin.site.register(ReadingList)
+
+
+
 
