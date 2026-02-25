e0343f5e717468a87bfe9cea6a6e350704eb9aff
fasharifi <sfsh1.sharifi@gmail.com>
2026-02-26 01:27:10 +0330

    add favorites url

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, CategoryViewSet, ContributionViewSet, ReadingListViewSet
+from .views import BookViewSet, CategoryViewSet, ContributionViewSet, ReadingListViewSet, FavoriteViewSet
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='books')
 router.register(r'categories', CategoryViewSet, basename='categories')
 router.register(r'contributions', ContributionViewSet, basename='contributions')
 router.register(r'readings', ReadingListViewSet, basename='readings')
+
+router.register(r'favorites', FavoriteViewSet, basename='favorite')
 
 
 urlpatterns = router.urls
